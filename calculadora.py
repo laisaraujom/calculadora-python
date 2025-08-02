cd822b7a9b446fd9a8abd542cdfce966a9da0966
Lais Moura <mouralais3@gmail.com>
2025-08-02 14:48:02 -0300

    versão com multiplicação

--- calculadora.py
+++ calculadora.py
@@ -1,17 +1,17 @@
-def soma(a, b):
+def mult(a, b):
     """
-    Realiza a soma de dois números.
+    Realiza o produto de dois números.
     
     Parâmetros:
     a (int/float): Primeiro número
     b (int/float): Segundo número
     
     Retorna:
-    int/float: Resultado da soma
+    int/float: Resultado do produto
     """
-    return a + b
+    return a * b
 
 if __name__ == "__main__":
-    print("Teste da função soma:")
-    print(f"2 + 3 = {soma(2, 3)}") 
-    print(f"7.2 + 3.8 = {soma(7.2, 3.8)}") 
+    print("Teste da função multiplicação:")
+    print(f"2 * 3 = {mult(2, 3)}") 
+    print(f"7.2 * 3.8 = {mult(7.2, 3.8)}") 
