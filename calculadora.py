4565dd181262530fcb6ea8ebbe0f4f7640c42ccc
Lais Moura <mouralais3@gmail.com>
2025-08-02 14:50:56 -0300

    multiplicação

--- calculadora.py
+++ calculadora.py
@@ -14,4 +14,4 @@ def mult(a, b):
 if __name__ == "__main__":
     print("Teste da função multiplicação:")
     print(f"2 * 3 = {mult(2, 3)}") 
-    print(f"7.2 * 3.8 = {mult(7.2, 3.8)}") 
+    print(f"7.5 * 3.8 = {mult(7.5, 3.8)}") 
